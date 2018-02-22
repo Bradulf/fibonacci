a54d4627b37ab36c83432137202db624cb7a0f70
Brad <bradkharris@gmail.com>
2018-02-22 10:47:41 -0700

    Initial commit

---
+++ fibonacci/fibonacci.cpp
@@ -0,0 +1,37 @@
+// fibonacci.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	const int NumsToCal = 5;
+	cout << "This program will calculate " << NumsToCal << " Fibonacci Numbers at a time" << endl;
+
+	int Num1 = 0, Num2 = 1;
+	char WantMore = '\0';
+	cout << Num1 << " " << Num2 << " ";
+
+	do {
+		for (int Index = 0; Index < NumsToCal; ++Index)
+		{
+			cout << Num1 + Num2 << " ";
+
+			int Num2Temp = Num2;
+			Num2 = Num1 + Num2;
+			Num1 = Num2Temp;
+		}
+
+		cout << endl << "do you want more numbers y/n?";
+		cin >> WantMore;
+
+	} while (WantMore == 'y');
+
+	cout << "goodbye" << endl;
+
+    return 0;
+}
+
